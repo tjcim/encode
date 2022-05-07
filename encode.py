a2c43d0a5cc7de59ad66890e0b4698e8bb9766ff
Trevor Christiansen <trevor@tjc.im>
2022-05-07 12:40:13 -0600

    Added some tests

--- encode.py
+++ encode.py
@@ -8,36 +8,74 @@ import urllib.parse
 
 def urlencode(string):
     """URL Encode String."""
-    return urllib.parse.quote(string)
+    result = []
+    for word in string:
+        result.append(urllib.parse.quote(word))
+    print("".join(result))
 
 
 def urldecode(string):
     """URL Decode String."""
-    return urllib.parse.unquote(string)
+    result = []
+    for word in string:
+        result.append(urllib.parse.unquote(word))
+    print("".join(result))
 
 
 def htmlencode(string):
     """HTML Encode String."""
-    return html.escape(string)
+    result = []
+    for word in string:
+        result.append(html.escape(word))
+    print("".join(result))
 
 
 def htmldecode(string):
     """HTML Decode String."""
-    return html.unescape(string)
+    result = []
+    for word in string:
+        result.append(html.unescape(word))
+    print("".join(result))
+
+
+def decimalencode(string):
+    """Encodes ascii characters into decimal, seperates words by new line."""
+    if len(string) == 1:
+        words = string[0].split()
+    else:
+        words = string
+    for word in words:
+        temp = []
+        for letter in word:
+            temp.append(str(ord(letter)))
+        print(" ".join(temp))
+
+
+def decimaldecode(string):
+    result = []
+    if type(string) == list and len(string) == 1 and " " in string[0]:
+        string = string[0].split()
+    for letter in string:
+        result.append(chr(int(letter)))
+    print("".join(result))
 
 
 def main(args):
     """Process args."""
     if args.decode:
         if args.url:
-            print(urldecode(args.string))
+            urldecode(args.string)
         if args.html:
-            print(htmldecode(args.string))
+            htmldecode(args.string)
+        if args.decimal:
+            decimaldecode(args.string)
     else:
         if args.url:
-            print(urlencode(args.string))
+            urlencode(args.string)
         if args.html:
-            print(htmlencode(args.string))
+            htmlencode(args.string)
+        if args.decimal:
+            decimalencode(args.string)
 
 
 if __name__ == "__main__":
@@ -47,7 +85,8 @@ if __name__ == "__main__":
     parser.add_argument("-d", "--decode", action="store_true", help="Decode instead of encode.")
     parser.add_argument("-u", "--url", action='store_true', help="URL encode special characters.")
     parser.add_argument("-t", "--html", action='store_true', help="HTML encode special characters.")
-    parser.add_argument("string", nargs="?", default=sys.stdin, help="Text that should be encoded.")
+    parser.add_argument("--decimal", action="store_true", help="Convert letter to decimal.")
+    parser.add_argument("string", nargs="+", default=sys.stdin, help="Text that should be encoded.")
     arglist = sys.argv[1:]
     if not sys.stdin.isatty():
         string = sys.stdin.read()
---
+++ test_encode.py
@@ -0,0 +1,41 @@
+import unittest
+import subprocess
+
+class TestURLMethods(unittest.TestCase):
+
+    def test_url_encode_argument(self):
+        result = subprocess.run(["encode.py", "-u", "?foo&bar"], capture_output=True)
+        self.assertEqual("%3Ffoo%26bar", result.stdout.decode().strip())
+
+    def test_url_encode_pipe(self):
+        echo_p = subprocess.Popen(
+            ["echo", "?foo&bar"],
+            stdout=subprocess.PIPE
+        )
+        encode_p = subprocess.Popen(
+            ["encode.py", "-u"],
+            stdin=echo_p.stdout,
+            stdout=subprocess.PIPE
+        )
+        echo_p.stdout.close()
+        echo_p.kill()
+        out, err = encode_p.communicate()
+        self.assertEqual("%3Ffoo%26bar", out.decode().strip())
+
+    def test_url_decode(self):
+        result = subprocess.run(["encode.py", "-u", "-d", "%3Ffoo%26bar"], capture_output=True)
+        self.assertEqual("?foo&bar", result.stdout.decode().strip())
+
+class TestDecimalMethods(unittest.TestCase):
+
+    def test_decimal_encode(self):
+        result = subprocess.run(["encode.py", "--decimal", "foo"], capture_output=True)
+        self.assertEqual("102 111 111", result.stdout.decode().strip())
+
+    def test_decimal_decode(self):
+        result = subprocess.run(["encode.py", "--decimal", "-d", "102 111 111"], capture_output=True)
+        self.assertEqual("foo", result.stdout.decode().strip())
+
+
+if __name__ == "__main__":
+    unittest.main()
